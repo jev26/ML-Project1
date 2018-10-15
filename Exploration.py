1e3bfc7b051abbfa308d7802830576f24934badd
veillard <jennifer.veillard@epfl.ch>
2018-10-15 16:09:36 +0200

    Feature Ranking based on Correlation - Strange at some points :/

--- Exploration.py
+++ Exploration.py
@@ -11,6 +11,8 @@ y, tX, ids = load_csv_data(data_path, sub_sample=False)
 #print(tX.shape) # (250000, 30) => 30 features
 #print(ids.shape) # (250000,)
 
+nSample, nFeature = tX.shape
+
 data1 = tX[y == -1]
 data2 = tX[y == 1]
 
@@ -19,23 +21,35 @@ print(data2.shape) # (85667, 30)
 
 print(data1[:,1].shape)
 
+## feature ranking according to the correlation between feature and label
 
+cor = numpy.zeros(nFeature)
+for iFeature in range(nFeature):
+    #print(tX[:,iFeature])
+    #print(y)
+    tmp = numpy.corrcoef(tX[:,iFeature],y)**2
+    cor[iFeature] = tmp[1][0]
+orderedPower = -numpy.sort(-cor)
+orderedInd = sorted(range(nFeature), key=lambda k: -cor[k])
+#print(orderedPower)
+print(orderedInd)
 
-# for each feature, display the histogram with color = label (y)
+## for each feature, display the histogram with color = label (y)
 
-for i in range(tX.shape[1]):
+if True:
+    for i in range(nFeature):
 
-    maximum = max(tX[:,i])
-    minumum = min(tX[:,i])
+        maximum = max(tX[:,i])
+        minimum = min(tX[:,i])
 
-    x = data1[:,i]
-    y = data2[:,i]
+        x = data1[:,i]
+        y = data2[:,i]
 
-    bins = numpy.linspace(minumum, maximum, 100)
+        bins = numpy.linspace(minimum, maximum, maximum-minimum)
 
-    pyplot.hist(x, bins, alpha=0.5, label='x')
-    pyplot.hist(y, bins, alpha=0.5, label='y')
-    pyplot.legend(loc='upper right')
-    pyplot.title('Feature n° ' + str(i))
-    pyplot.show()
+        pyplot.hist(x, bins, alpha=0.5, label='x')
+        pyplot.hist(y, bins, alpha=0.5, label='y')
+        pyplot.legend(loc='upper right')
+        pyplot.title('Feature n° ' + str(i))
+        pyplot.show()
 
