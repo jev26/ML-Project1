5ebe006d9b25c6cc6faa350aea04dd59ccfea157
Loic Nguyen <loic.nguyen@epfl.ch>
2018-10-16 18:09:14 +0200

    clean a bit

--- Exploration.py
+++ Exploration.py
@@ -5,6 +5,28 @@ from implementation import *
 
 import random
 
+
+
+#TODO: documentation
+def data_cleaning(tX, damagedFeature, i):
+    #exclude values = -999
+    tX_index_wash1 = tX[:, damagedFeature[i]] != -999
+    tX_wash1 = tX[tX_index_wash1, damagedFeature[i]]
+
+    #exclude value > 5*standard deviation
+    tX_wash1_std = np.std(tX_wash1)
+    tX_index_wash2 = (tX_wash1 <= tX_wash1_std * 5) & (-tX_wash1_std * 5 <= tX_wash1)
+    tX_tmp = tX_wash1[tX_index_wash2]
+
+
+    y_tmp1 = y[tX_index_wash1]
+    y_tmp = y_tmp1[tX_index_wash2]
+
+    return tX_tmp, y_tmp
+
+
+
+
 data_path = 'data/train.csv'
 y, tX, ids = load_csv_data(data_path, sub_sample=False)
 
@@ -13,6 +35,7 @@ y, tX, ids = load_csv_data(data_path, sub_sample=False)
 # print(ids.shape) => (250000,)
 
 nSample, nFeature = tX.shape
+
 """
 data1 = tX[y == -1]
 data2 = tX[y == 1]
@@ -35,14 +58,15 @@ for i in range(nFeature):
     print('Feature n° ' + str(i) + " " + str(tmp))
     print('Ratio ' + str(tmp/nSample))
 """
+
 # feature ranking according to the correlation between feature and label
 damagedFeature = np.array([5, 12, 26, 27, 28, 23, 24, 25, 22, 29])
 cor = np.zeros(nFeature)
+
 ## correlation when -999 are dropped
 cor = np.zeros(nFeature)
 print(nSample)
 for iFeature in range(nFeature):
-
     # discard -999
     sel = tX[:, iFeature] != -999
     tX_tmp = tX[sel, iFeature]
@@ -58,29 +82,26 @@ for iFeature in range(nFeature):
 
 orderedPower = -np.sort(-cor)
 orderedInd = sorted(range(nFeature), key=lambda k: -cor[k])
+
 #print(orderedPower)
-print(orderedInd)
+#print(orderedInd)
+
+
 # for each feature, display the histogram with color = label (y)
 
 damagedFeature0 = orderedInd
 for i in range(len(damagedFeature0)):
-    tX_index_wash1 = tX[:, damagedFeature0[i]] != -999
-    tX_wash1 = tX[tX_index_wash1,damagedFeature0[i]]
-    tX_wash1_std = np.std(tX_wash1)
-    tX_index_wash2 = (tX_wash1 <= tX_wash1_std*5) & (-tX_wash1_std*5 <= tX_wash1)
-    tX_tmp = tX_wash1[tX_index_wash2]
-    y_tmp1 = y[tX_index_wash1]
-    y_tmp = y_tmp1[tX_index_wash2]
+
+    tX_tmp, y_tmp = data_cleaning(tX, damagedFeature0, i)
+
     maximum = max(tX_tmp)
     minimum = min(tX_tmp)
-
-    print(damagedFeature0[i])
-    print(tX_tmp.shape)
-    print(np.size(tX_tmp))
 
     data1 = tX_tmp[y_tmp == -1]
     data2 = tX_tmp[(y_tmp == 1)]
 
+
+    #plot the figure
 
     bins = np.linspace(minimum, maximum, 80)
     #plt.figure(i)
