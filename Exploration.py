45f05bfc8ca0b010b9f72c0a5311d31e06a8d9a2
EvilTuts <arthur.barakat@epfl.ch>
2018-10-16 17:15:19 +0200

    Retirer valeur manquante + histogram correcte

--- Exploration.py
+++ Exploration.py
@@ -13,7 +13,7 @@ y, tX, ids = load_csv_data(data_path, sub_sample=False)
 # print(ids.shape) => (250000,)
 
 nSample, nFeature = tX.shape
-
+"""
 data1 = tX[y == -1]
 data2 = tX[y == 1]
 
@@ -21,8 +21,9 @@ print(data1.shape) # (164333, 30)
 print(data2.shape) # (85667, 30)
 
 print(data1[:,1].shape)
-
+"""
 # count the number of -999 per feature
+"""
 for i in range(nFeature):
     tmp = sum(tX[:, i] == -999)
     print('Feature n° ' + str(i) + " " + str(tmp))
@@ -33,38 +34,60 @@ for i in range(nFeature):
     tmp = sum(tX[:, i] == 0)
     print('Feature n° ' + str(i) + " " + str(tmp))
     print('Ratio ' + str(tmp/nSample))
-
+"""
 # feature ranking according to the correlation between feature and label
 damagedFeature = np.array([5, 12, 26, 27, 28, 23, 24, 25, 22, 29])
 cor = np.zeros(nFeature)
+## correlation when -999 are dropped
+cor = np.zeros(nFeature)
+print(nSample)
 for iFeature in range(nFeature):
+
+    # discard -999
+    sel = tX[:, iFeature] != -999
+    tX_tmp = tX[sel, iFeature]
+    # print(tX_tmp.shape)
+    y_tmp = y[sel]
+    # print(y_tmp.shape)
+
     # print(tX[:,iFeature])
     # print(y)
-    if iFeature not in damagedFeature:
-        tmp = np.corrcoef(tX[:,iFeature],y)**2
-        cor[iFeature] = tmp[1][0]
+    tmp = np.corrcoef(tX_tmp,y_tmp)**2
+    #print(tmp)
+    cor[iFeature] = tmp[1][0]
+
 orderedPower = -np.sort(-cor)
 orderedInd = sorted(range(nFeature), key=lambda k: -cor[k])
-print(orderedPower)
+#print(orderedPower)
 print(orderedInd)
-
 # for each feature, display the histogram with color = label (y)
 
 damagedFeature0 = orderedInd
 for i in range(len(damagedFeature0)):
+    tX_index_wash1 = tX[:, damagedFeature0[i]] != -999
+    tX_wash1 = tX[tX_index_wash1,damagedFeature0[i]]
+    tX_wash1_std = np.std(tX_wash1)
+    tX_index_wash2 = (tX_wash1 <= tX_wash1_std*5) & (-tX_wash1_std*5 <= tX_wash1)
+    tX_tmp = tX_wash1[tX_index_wash2]
+    y_tmp1 = y[tX_index_wash1]
+    y_tmp = y_tmp1[tX_index_wash2]
+    maximum = max(tX_tmp)
+    minimum = min(tX_tmp)
 
-    maximum = max(tX[:,damagedFeature0[i]])
-    minimum = min(tX[:,damagedFeature0[i]])
+    print(damagedFeature0[i])
+    print(tX_tmp.shape)
+    print(np.size(tX_tmp))
 
-    print(i)
-    x = data1[:,damagedFeature0[i]]
-    y = data2[:,damagedFeature0[i]]
+    data1 = tX_tmp[y_tmp == -1]
+    data2 = tX_tmp[(y_tmp == 1)]
 
-    bins = np.linspace(minimum, maximum, maximum-minimum)
+
+    bins = np.linspace(minimum, maximum, 80)
     plt.figure(i)
-    plt.hist(x, bins, alpha=0.5, label='x')
-    plt.hist(y, bins, alpha=0.5, label='y')
+    plt.hist(data1, bins, alpha=0.5, label='x')
+    plt.hist(data2, bins, alpha=0.5, label='y')
+    plt.xlim(minimum,maximum)
     plt.legend(loc='upper right')
-    plt.title('Feature n° ' + str(i))
+    plt.title('Feature n° ' + str(damagedFeature0[i]))
     plt.show()
 
