d7f057bcd7bd92b2db3de07bc46fbf5ecf4f6f42
Loic Nguyen <loic.nguyen@epfl.ch>
2018-10-20 01:06:09 +0200

    add comments on poly features

--- poly_features.py
+++ poly_features.py
@@ -3,17 +3,25 @@ import itertools
 
 
 def polynomial_features(X, degree):
+
+    #get the number of samples and features from the X matrix
     nb_samples, nb_features = X.shape
 
+    #create an iterator that lets us iterate over all combinations of numbers from 0 to nb_features-1
     combi = itertools.chain.from_iterable(
         itertools.combinations_with_replacement(range(nb_features), i) for i in range(degree + 1))
+
+    #use that iterator to get the total number of features of the output
     nb_output = sum(1 for _ in combi)
 
+    #initiate an empty array for the output
     PF = np.empty([nb_samples, nb_output])
 
+    #instantiate the iterator again
     combi = itertools.chain.from_iterable(
         itertools.combinations_with_replacement(range(nb_features), i) for i in range(degree + 1))
 
+    #create the polynomial features by iterating and multipliying the columns
     for a, b in enumerate(combi):
         PF[:, a] = X[:, b].prod(1)
 
